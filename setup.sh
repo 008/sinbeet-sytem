bd603d0c1ec439f115b1960d45d4e2f7d2de3027
BEET <averus008@gmail.com>
2019-10-28 17:53:20 +0300

    updatenodecron

--- setup.sh
+++ setup.sh
@@ -402,7 +402,63 @@ crontab -l | { cat; echo "*/5 * * * * /root/infinitynode_surveyor.sh"; } | cront
 }
 
 
+updatenodecron() {
 
+cat << EOF >> updatenodecron.s
+#!/bin/bash
+
+    if [ -f "sin/cur" ]; then 
+        curnodever=$(cat sin/cur) 
+        rm sin/cur
+		wget -O sin/cur link
+		newnodever=$(cat sin/cur)
+		
+		    if [ "$curnodever" -eq "$newnodever" ]; then
+			echo "update check no new ver" >> status
+			echo "update check no new ver" >> .sin/debug.log
+			exit
+			else
+			echo "updating node ver..." >> .sin/debug.log
+			echo "updating node ver..." >> status
+			
+			service sind stop
+			killall -9 sind
+			sleep 1
+
+			rm -rf /usr/local/bin/sin-cli
+			rm -rf /usr/local/bin/sind
+			rm -rf ubu18.*
+			rm -rf ubu16.*
+			
+			wget https://github.com/008/sinbeet-sytem/raw/master/current/ubu18.zip
+			
+			unzip ubu*
+			chmod +x sin*
+			install -c sin-cli /usr/local/bin/sin-cli
+			install -c sind /usr/local/bin/sind
+			service sind start
+			echo "updating node DONE ver date $newnodever" >> .sin/debug.log
+			echo "updating node DONE ver date $newnodever" >> status
+			fi
+			
+			
+		
+    else
+wget cur
+    fi
+	
+EOF
+	
+
+chmod +x updatenodecron.sh
+crontab -l | { cat; echo "0 1 * * * /root/updatenodecron.sh"; } | crontab -
+
+
+
+	echo "updatenodecron DONE" >> .sin/debug.log
+	
+	
+}
 
 
 
@@ -458,6 +514,7 @@ install -c sind /usr/local/bin/sind
     serviceInstall
     startWallet
 	cron
+	updatenodecron
     end
 	
 
