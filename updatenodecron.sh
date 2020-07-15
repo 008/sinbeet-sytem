e929a4a440808372fd2871a7bbf7b59ffa1c6c65
BEET <averus008@gmail.com>
2020-07-15 12:21:04 +0300

    Update updatenodecron.sh

--- updatenodecron.sh
+++ updatenodecron.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+﻿#!/bin/bash
 
-    if [ -f "sin/cur" ]; then 
+    if [ -f ".sin/cur" ]; then 
 	
 	sleep $((RANDOM % 7200))
 	
-        curnodever=$(cat sin/cur)
-        rm sin/cur
-		wget -O sin/cur https://raw.githubusercontent.com/008/sinbeet-sytem/master/cur
-		newnodever=$(cat sin/cur)
+        curnodever=$(cat .sin/cur)
+        rm .sin/cur
+		wget -O .sin/cur https://raw.githubusercontent.com/008/sinbeet-sytem/master/cur
+		newnodever=$(cat .sin/cur)
 		
 		    if [ "$curnodever" -eq "$newnodever" ]; then
 			echo "update check no new ver" >> status
@@ -25,22 +25,24 @@
 			rm -rf /usr/local/bin/sind
 			rm -rf sin-cli
 			rm -rf sind
-			rm -rf ubu18.*
-			rm -rf ubu16.*
+			rm -rf linux*
+			#rm -rf ubu18.*
+			#rm -rf ubu16.*
 			
-			wget https://github.com/008/sinbeet-sytem/raw/master/current/ubu18.zip
+			#wget https://github.com/008/sinbeet-sytem/raw/master/current/ubu18.zip
+			wget https://github.com/008/sinbeet-sytem/blob/master/$newnodever/linux.zip
 			
-			if [ ! -f "ubu18.zip" ]; then 
+			if [ ! -f "linux.zip" ]; then 
 			echo "download node fail, will try later" >> .sin/debug.log
 			echo "download node fail, will try later" >> status
 			exit
 			fi
 			
-			unzip ubu*
+			unzip linux*
 			chmod +x sin*
 			install -c sin-cli /usr/local/bin/sin-cli
 			install -c sind /usr/local/bin/sind
-			service sind start
+			service sind start || sind
 			echo "updating node DONE ver date $newnodever" >> .sin/debug.log
 			echo "updating node DONE ver date $newnodever" >> status
 			fi
@@ -48,6 +50,6 @@
 			
 		
     else
-	wget -O sin/cur https://raw.githubusercontent.com/008/sinbeet-sytem/master/cur
+	wget -O .sin/cur https://raw.githubusercontent.com/008/sinbeet-sytem/master/cur
     fi
 	
