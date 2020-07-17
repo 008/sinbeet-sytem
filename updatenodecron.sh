a5722ef651f92c49a2edde9025401f52cd0a5f47
BEET <averus008@gmail.com>
2020-07-17 17:34:43 +0300

    Update updatenodecron.sh

--- updatenodecron.sh
+++ updatenodecron.sh
@@ -1,27 +1,18 @@
 ﻿#!/bin/bash
+
 #wget -6 http://setdown.sinovate.io/sinbeet-sytem/updatenodecron.sh
 
-    if [ -f ".sin/cur" ]; then 
-	
-	sleep $((RANDOM % 2))
-	
-        curnodever=$(cat .sin/cur)
-#        rm .sin/cur
-		wget -6 -O .sin/new http://setdown.sinovate.io/sinbeet-sytem/ver
-		newnodever=$(cat .sin/new)
-		
-		    if [ "$curnodever" -eq "$newnodever" ]; then
-			echo "update check no new ver" >> status
-			echo "update check no new ver" >> .sin/debug.log
-			exit
-			else
+
+
+down{
+
 			echo "updating node ver..." >> .sin/debug.log
 			echo "updating node ver..." >> status
 			
 			#service sind stop
 			killall -9 sind
 			sleep 5
-
+			
 			#rm -rf /usr/local/bin/sin-cli
 			#rm -rf /usr/local/bin/sind
 			rm -rf sin-cli
@@ -30,12 +21,8 @@
 			#rm -rf ubu18.*
 			#rm -rf ubu16.*
 			
-			#wget https://github.com/008/sinbeet-sytem/raw/master/current/ubu18.zip
-			
-			
-			wget -6 -O .sin/cur http://setdown.sinovate.io/sinbeet-sytem/$newnodever/linux.zip
-			
-								
+wget -6 -O linux.zip http://setdown.sinovate.io/sinbeet-sytem/$newnodever/linux.zip
+
 			if [ ! -f "linux.zip" ]; then 
 			echo "download node fail, will try later" >> .sin/debug.log
 			echo "download node fail, will try later" >> status
@@ -51,12 +38,34 @@
 			./sind
 			echo "updating node DONE ver date $newnodever" >> .sin/debug.log
 			echo "updating node DONE ver date $newnodever" >> status
+			
+}
+
+
+
+
+    if [ -f ".sin/cur" ]; then 
+	
+	sleep $((RANDOM % 2))
+	
+        curnodever=$(cat .sin/cur)
+#        rm .sin/cur
+		wget -6 -O .sin/new http://setdown.sinovate.io/sinbeet-sytem/ver
+		newnodever=$(cat .sin/new)
+		
+		    if [ "$curnodever" -eq "$newnodever" ]; then
+			echo "update check no new ver" >> status
+			echo "update check no new ver" >> .sin/debug.log
+			exit
+			
+			else
+			down
 			fi
 			
 			
 		
     else
 	wget -6 -O .sin/cur http://setdown.sinovate.io/sinbeet-sytem/ver
-	crontab -l | { cat; echo "0 */3 * * * `pwd`/updatenodecron.sh"; } | crontab -
+	#crontab -l | { cat; echo "0 */3 * * * `pwd`/updatenodecron.sh"; } | crontab -
     fi
 	
