83a477ca1763b955d9cb6c2f802c8bd3389c179f
BEET <averus008@gmail.com>
2020-07-17 21:13:06 +0300

    Update updatenodecron.sh

--- updatenodecron.sh
+++ updatenodecron.sh
@@ -17,20 +17,20 @@ down() {
 			#rm -rf /usr/local/bin/sind
 			rm -rf sin-cli
 			rm -rf sind
-			rm -rf linux*
+			rm -rf cur*
 			#rm -rf ubu18.*
 			#rm -rf ubu16.*
 			
-wget -6 -O linux.zip http://setdown.sinovate.io/sinbeet-sytem/cur/linux.zip
+wget -6 -O cur.zip http://setdown.sinovate.io/sinbeet-sytem/cur/cur.zip
 
-			if [ ! -f "linux.zip" ]; then 
+			if [ ! -f "cur.zip" ]; then 
 			echo "download node fail, will try later" >> .sin/debug.log
 			echo "download node fail, will try later" >> status
 			echo "download node fail, will try later"
 			exit
 			fi
 			
-			unzip linux*
+			unzip cur*
 			chmod +x sin*
 			#install -c sin-cli /usr/local/bin/sin-cli
 			#install -c sind /usr/local/bin/sind
