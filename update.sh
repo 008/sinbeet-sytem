8dc6f4c40baffd76873bdcaccfb8665351913cf0
BEET <averus008@gmail.com>
2020-07-22 17:24:07 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 ##wget -6 http://setdown.sinovate.io/sinbeet-sytem/update.sh
 #rm update.sh ;wget -6 http://setdown.sinovate.io/sinbeet-sytem/update.sh; bash update.sh
@@ -79,3 +79,4 @@ wget -6 -O cur.zip http://setdown.sinovate.io/sinbeet-sytem/cur/cur.zip
 	down
 	#crontab -l | { cat; echo "0 */3 * * * `pwd`/update.sh"; } | crontab -
     fi
+	
