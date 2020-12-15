d6cfd3707ab1fa1144d6a395550a35bb36fd1f64
BEET <averus008@gmail.com>
2020-12-15 20:23:34 +0200

    log sleep 598

--- update.sh
+++ update.sh
@@ -428,7 +428,7 @@ echo "`date` start seq done" >> .sin/debug.log
 while sleep 480; do sinerror3; done & #daemon running check
 while sleep 901; do sinerror4; done & #blockcount check
 #while sleep 174; do sinerror2; done &
-while sleep 1861; do sinlog; done &
+while sleep 598; do sinlog; done &
 while sleep 86399; do sinstop;sinstart;echo "*************** `date` node restart" >> .sin/debug.log;echo "*************** `date` node restart" >> status; done &
 sleep 30 && sinerror1 &
 sleep 301 && createblockmark &
