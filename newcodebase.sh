6824ecf578c50e7eff0ca6f956b2b1d135e951b1
BEET <averus008@gmail.com>
2022-08-21 16:27:24 +0300

    -maxconnections=30

--- newcodebase.sh
+++ newcodebase.sh
@@ -263,7 +263,7 @@ echo "no bootstrap.7z starting daemon"
 
 
 export MALLOC_ARENA_MAX=1
-./sind -daemon -debug=0 -staking=0 -rpcbind=:: -wallet=01 -dbcache=5 -maxmempool=50 -mempoolexpiry=4 -rpcthreads=2
+./sind -daemon -debug=0 -staking=0 -rpcbind=:: -wallet=01 -dbcache=5 -maxmempool=50 -mempoolexpiry=4 -rpcthreads=2 -maxconnections=30
 # -maxconnections=4
 
 
